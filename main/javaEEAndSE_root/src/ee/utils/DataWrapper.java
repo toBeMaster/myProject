7f794de915c1c95af44672b64e1b887ff6dff0e4
toBeMaster <1242747708@qq.com>
2018-01-12 11:12:29 +0800

    文件处理

--- main/javaEEAndSE_root/src/ee/utils/DataWrapper.java
+++ main/javaEEAndSE_root/src/ee/utils/DataWrapper.java
@@ -15,7 +15,7 @@ import ee.model.ResultMsgModel;
 
 public class DataWrapper {
 	/**
-	 * 转换成json格式
+	 * 杞崲鎴恓son鏍煎紡
 	 * 
 	 * @param obj
 	 */
@@ -31,19 +31,36 @@ public class DataWrapper {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * 转换成json格式
+	 * 
+	 * @param obj
+	 */
+	public static void putJson(Object obj) {
+		HttpServletResponse resp = ServletActionContext.getResponse();
+		String json = JSON.toJSONString(obj,
+				SerializerFeature.DisableCircularReferenceDetect);
+		resp.setContentType("text/json;charset=utf-8");
+		PrintWriter out = null;
+		try {
+			out = resp.getWriter();
+			out.print(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	@Test
 	public void testJsonDate() {
 		ResultMsgModel msg = new ResultMsgModel();
-		msg.setMsg("hello,你好");
+		msg.setMsg("hello,浣犲ソ");
 		msg.setSuccess(true);
 		msg.setObj(new Date());
-		// 输出毫秒值
+		// 杈撳嚭姣鍊�
 		System.out.println(JSON.toJSONString(msg));
-		// 默认格式为yyyy-MM-dd HH:mm:ss
+		// 榛樿鏍煎紡涓簓yyy-MM-dd HH:mm:ss
 		System.out.println(JSON.toJSONString(msg,
 				SerializerFeature.WriteDateUseDateFormat));
-		// 根据自定义格式输出日期
+		// 鏍规嵁鑷畾涔夋牸寮忚緭鍑烘棩鏈�
 		System.out.println(JSON.toJSONStringWithDateFormat(msg, "yyyy-MM-dd",
 				SerializerFeature.WriteDateUseDateFormat));
 	}
